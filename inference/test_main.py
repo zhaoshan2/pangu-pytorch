4e083398156ed1e9eef685bc8eb393e6ce6ba319
EliasKng <elias.koenig@student.kit.edu>
2024-09-16 10:51:03 +0200

    Improve code quality

--- inference/test_main.py
+++ inference/test_main.py
@@ -1,6 +1,7 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from era5_data import utils, utils_data
 from era5_data.config import cfg
 from models.pangu_model import PanguModel
@@ -12,59 +13,67 @@ from models.pangu_sample import test
 import argparse
 import time
 import logging
-import pandas as pd
-import torch
+
 if __name__ == "__main__":
     """
     check the re-implemented model performance
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--type_net', type=str, default="reproduce_mask0")
+    parser.add_argument("--type_net", type=str, default="reproduce_mask0")
     args = parser.parse_args()
-    starts  = time.time()
-   
+    starts = time.time()
+    print("Hello")
+
     PATH = cfg.PG_INPUT_PATH
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Predicting on {device}")
 
     output_path = os.path.join(cfg.PG_OUT_PATH, args.type_net, str(cfg.PG.HORIZON))
     utils.mkdirs(output_path)
 
     logger_name = args.type_net + str(cfg.PG.HORIZON)
-    utils.logger_info(logger_name, os.path.join(output_path, logger_name + '_test.log'))
+    utils.logger_info(logger_name, os.path.join(output_path, logger_name + "_test.log"))
 
     logger = logging.getLogger(logger_name)
 
-    test_dataset = utils_data.NetCDFDataset(nc_path=PATH,
-                                data_transform=None,
-                                training=False,
-                                validation = False,
-                                startDate = cfg.PG.TEST.START_TIME,
-                                endDate= cfg.PG.TEST.END_TIME,
-                                freq=cfg.PG.TEST.FREQUENCY,
-                                horizon=cfg.PG.HORIZON)
+    test_dataset = utils_data.NetCDFDataset(
+        nc_path=PATH,
+        data_transform=None,
+        training=False,
+        validation=False,
+        startDate=cfg.PG.TEST.START_TIME,
+        endDate=cfg.PG.TEST.END_TIME,
+        freq=cfg.PG.TEST.FREQUENCY,
+        horizon=cfg.PG.HORIZON,
+    )
 
-    test_dataloader = data.DataLoader(dataset=test_dataset, batch_size=cfg.PG.TEST.BATCH_SIZE,
-                                            drop_last=True, shuffle=False, num_workers=0, pin_memory=False)
-
-
+    test_dataloader = data.DataLoader(
+        dataset=test_dataset,
+        batch_size=cfg.PG.TEST.BATCH_SIZE,
+        drop_last=True,
+        shuffle=False,
+        num_workers=0,
+        pin_memory=False,
+    )
 
     torch.set_num_threads(16)
 
     model = PanguModel(device=device).to(device)
 
     checkpoint = torch.load(cfg.PG.BENCHMARK.PRETRAIN_24_torch, map_location=device)
-    model.load_state_dict(checkpoint['model'])
+    model.load_state_dict(checkpoint["model"])
 
     logger.info("Begin Test")
-    msg = '\n'
+    msg = "\n"
     msg += utils.torch_summarize(model, show_weights=False)
     logger.info(msg)
     output_path = os.path.join(output_path, "test")
     utils.mkdirs(output_path)
 
-    test(test_loader=test_dataloader,
-             model = model,
-             device=model.device,
-             res_path = output_path)
+    test(
+        test_loader=test_dataloader,
+        model=model,
+        device=model.device,
+        res_path=output_path,
+    )
--- models/pangu_model.py
+++ models/pangu_model.py
@@ -1,98 +1,108 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from torch import nn
 import torch
 from models.layers import *
 from collections import OrderedDict
-import torch.utils.checkpoint as checkpoint
-from era5_data import utils, utils_data
 
 
 class PanguModel(nn.Module):
-  def __init__(self, depths = [2,6,6,2], num_heads = [6, 12, 12, 6], dims = [192, 384, 384, 192], patch_size = (2, 4, 4), device=None):
-    super(PanguModel, self).__init__()
-  
-    # Patch embedding
-    self.device = device
+    def __init__(
+        self,
+        depths=[2, 6, 6, 2],
+        num_heads=[6, 12, 12, 6],
+        dims=[192, 384, 384, 192],
+        patch_size=(2, 4, 4),
+        device=None,
+    ):
+        super(PanguModel, self).__init__()
 
-    # self._input_layer = PatchEmbedding(patch_size, dims[0], device=self.device)
-    self._input_layer = PatchEmbedding_pretrain(patch_size, dims[0])
-    self.downsample = DownSample(dims[0])
+        # Patch embedding
+        self.device = device
 
-    dpr = [x.item() for x in torch.linspace(0, 0.2, sum(depths))]
-    # build layers
-    self.num_layers = len(depths)
-    
-    layer_list = OrderedDict()
-    for i_layer in range(self.num_layers):
-        layer_list['EarthSpecificLayer{}'.format(i_layer)] = EarthSpecificLayer(
-          depth = depths[i_layer],
-          dim = dims[i_layer], 
-          drop_path_ratio_list = dpr[sum(depths[:i_layer]):sum(depths[:i_layer + 1])],
-          heads = num_heads[i_layer],
-          use_checkpoint = self.training,
-          device = self.device)
-    self.layers = nn.Sequential(layer_list)
+        # self._input_layer = PatchEmbedding(patch_size, dims[0], device=self.device)
+        self._input_layer = PatchEmbedding_pretrain(patch_size, dims[0])
+        self.downsample = DownSample(dims[0])
 
-    self.upsample = UpSample(dims[-2], dims[-1])
+        dpr = [x.item() for x in torch.linspace(0, 0.2, sum(depths))]
+        # build layers
+        self.num_layers = len(depths)
 
-    # Patch Recovery
-    # self._output_layer = PatchRecovery(dims[-2])
-    self._output_layer = PatchRecovery_pretrain(dims[-2])
-    self.apply(self._init_weights)
+        layer_list = OrderedDict()
+        for i_layer in range(self.num_layers):
+            layer_list["EarthSpecificLayer{}".format(i_layer)] = EarthSpecificLayer(
+                depth=depths[i_layer],
+                dim=dims[i_layer],
+                drop_path_ratio_list=dpr[
+                    sum(depths[:i_layer]) : sum(depths[: i_layer + 1])
+                ],
+                heads=num_heads[i_layer],
+                use_checkpoint=self.training,
+                device=self.device,
+            )
+        self.layers = nn.Sequential(layer_list)
 
-  def _init_weights(self, m):
-    if isinstance(m, nn.Linear):
-        trunc_normal_(m.weight, std=.02)
-        if isinstance(m, nn.Linear) and m.bias is not None:
+        self.upsample = UpSample(dims[-2], dims[-1])
+
+        # Patch Recovery
+        # self._output_layer = PatchRecovery(dims[-2])
+        self._output_layer = PatchRecovery_pretrain(dims[-2])
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=0.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
             nn.init.constant_(m.bias, 0)
-    elif isinstance(m, nn.LayerNorm):
-        nn.init.constant_(m.bias, 0)
-        nn.init.constant_(m.weight, 1.0)  
+            nn.init.constant_(m.weight, 1.0)
 
-  def forward(self, input, input_surface, statistics, maps, const_h):
-    '''Backbone architecture'''
-    # Embed the input fields into patches
-    # input:(B, N, Z, H, W) ([1, 5, 13, 721, 1440])input_surface(B,N,H,W)([1, 4, 721, 1440])
-    # x = checkpoint.checkpoint(self._input_layer, input, input_surface)
-    x = self._input_layer(input, input_surface, statistics, maps, const_h) #([1, 521280, 192]) [B, spatial, C]
+    def forward(self, input, input_surface, statistics, maps, const_h):
+        """Backbone architecture"""
+        # Embed the input fields into patches
+        # input:(B, N, Z, H, W) ([1, 5, 13, 721, 1440])input_surface(B,N,H,W)([1, 4, 721, 1440])
+        # x = checkpoint.checkpoint(self._input_layer, input, input_surface)
+        x = self._input_layer(
+            input, input_surface, statistics, maps, const_h
+        )  # ([1, 521280, 192]) [B, spatial, C]
 
-    # Encoder, composed of two layers
-    # Layer 1, shape (8, 360, 181, C), C = 192 as in the original paper
-    
-    x = self.layers[0](x, 8, 181, 360)
+        # Encoder, composed of two layers
+        # Layer 1, shape (8, 360, 181, C), C = 192 as in the original paper
 
-    # Store the tensor for skip-connection
-    skip = x 
+        x = self.layers[0](x, 8, 181, 360)
 
-    # Downsample from (8, 360, 181) to (8, 180, 91)
-    x = self.downsample(x, 8, 181, 360)
+        # Store the tensor for skip-connection
+        skip = x
 
-    x = self.layers[1](x, 8, 91, 180)
-    # Decoder, composed of two layers
-    # Layer 3, shape (8, 180, 91, 2C), C = 192 as in the original paper
-    x = self.layers[2](x, 8, 91, 180)
+        # Downsample from (8, 360, 181) to (8, 180, 91)
+        x = self.downsample(x, 8, 181, 360)
 
-    # Upsample from (8, 180, 91) to (8, 360, 181)
-    x = self.upsample(x)
+        x = self.layers[1](x, 8, 91, 180)
+        # Decoder, composed of two layers
+        # Layer 3, shape (8, 180, 91, 2C), C = 192 as in the original paper
+        x = self.layers[2](x, 8, 91, 180)
 
-    # Layer 4, shape (8, 360, 181, 2C), C = 192 as in the original paper
-    x = self.layers[3](x, 8, 181, 360) #([1, 521280, 192])
+        # Upsample from (8, 180, 91) to (8, 360, 181)
+        x = self.upsample(x)
+
+        # Layer 4, shape (8, 360, 181, 2C), C = 192 as in the original paper
+        x = self.layers[3](x, 8, 181, 360)  # ([1, 521280, 192])
+
+        # Skip connect, in last dimension(C from 192 to 384)
+        x = torch.cat((skip, x), dim=-1)
+
+        # Recover the output fields from patches
+        # output, output_surface = checkpoint.checkpoint(self._output_layer, x, 8, 181, 360)
+        output, output_surface = self._output_layer(x, 8, 181, 360)
+
+        return output, output_surface
 
 
-    # Skip connect, in last dimension(C from 192 to 384)
-    x = torch.cat((skip, x), dim=-1)
-
-    # Recover the output fields from patches
-    # output, output_surface = checkpoint.checkpoint(self._output_layer, x, 8, 181, 360)
-    output, output_surface = self._output_layer(x, 8, 181, 360)
-
-    return output, output_surface
-
-
-if __name__ == '__main__':
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = PanguModel(device=device).to(device)
 
@@ -100,17 +110,13 @@ if __name__ == '__main__':
     x_upper = torch.randn((1, 5, 13, 721, 1440)).to(device)
     # output, output_surface = model(x_upper, x_surface)
 
+    # aux_constants = utils.loadAllConstants(
+    #     device=device)  # 'weather_statistics','weather_statistics_last','constant_maps','tele_indices','variable_weights'
+    # # Note the input and target need to be normalized (done within the function)
+    # # Call the model and get the output
+    # output, output_surface = model(x_upper, x_surface, aux_constants['weather_statistics'],
+    #                                 aux_constants['constant_maps'],
+    #                                 aux_constants['const_h'])  # (1,5,13,721,1440)
 
-    aux_constants = utils.loadAllConstants(
-        device=device)  # 'weather_statistics','weather_statistics_last','constant_maps','tele_indices','variable_weights'
-    # Note the input and target need to be normalized (done within the function)
-    # Call the model and get the output
-    output, output_surface = model(x_upper, x_surface, aux_constants['weather_statistics'],
-                                    aux_constants['constant_maps'],
-                                    aux_constants['const_h'])  # (1,5,13,721,1440)
-
-
-
-    # print(output)
-    print(output.shape)
-   
+    # # print(output)
+    # print(output.shape)
