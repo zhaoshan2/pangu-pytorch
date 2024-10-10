1646afa53fdb1fd29151b1c9a310d7d206333a6c
EliasKng <elias.koenig@student.kit.edu>
2024-10-10 15:27:02 +0200

    Fix adam error in lora tune

--- finetune/lora_tune.py
+++ finetune/lora_tune.py
@@ -9,6 +9,7 @@ from models.pangu_model import PanguModel
 from torch import nn
 import torch
 from torch.utils import data
+from torch.optim.adam import Adam
 from models.pangu_sample import test, train
 import argparse
 import time
@@ -170,7 +171,7 @@ if __name__ == "__main__":
     module_copy = copy.deepcopy(model)  # we keep a copy of the original model for later
 
     peft_model = get_peft_model(model, config)
-    optimizer = torch.optim.Adam(
+    optimizer = Adam(
         peft_model.parameters(),
         lr=cfg.PG.TRAIN.LR,
         weight_decay=cfg.PG.TRAIN.WEIGHT_DECAY,
